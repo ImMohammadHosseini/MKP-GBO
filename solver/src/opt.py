c34d0b61013e2ed0749b760bdb23e4612121d8f2
ImMohammadHosseini <sayedmh50@gmail.com>
2023-08-21 16:37:35 +0330

    personilize opt from main sorce in COSCO repo

--- solver/src/opt.py
+++ solver/src/opt.py
@@ -15,21 +15,17 @@ def convertToOneHot(dat, fixed_part, ksNum):
     return new_dat_oneHot
 
 def opt(observed, model, ksNum):
-    print(observed.data[:3])
     observed.requires_grad = True
     optimizer = torch.optim.Adam([observed] , lr=0.8)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
-    iteration = 0; equal = 0; z_old = 100
+    iteration = 0; equal = 0
     fixed_part = deepcopy(observed.data[:,0:-(ksNum+1)])
     while iteration < 200:
         alloc_old = deepcopy(observed.data[:,-(ksNum+1):])
         z = model(observed)
-        #z=torch.tensor([100.],requires_grad=True)
         optimizer.zero_grad(); z.backward(); optimizer.step(); scheduler.step()
         observed.data = convertToOneHot(observed.data, fixed_part, ksNum)
         equal = equal + 1 if torch.all(alloc_old.eq(observed.data[:,-(ksNum+1):])) else 0
         if equal > 30: break
-        iteration += 1; z_old = z.item()
-        
+        iteration += 1
     observed.requires_grad = False 
-    return observed.data, iteration, model(observed)
