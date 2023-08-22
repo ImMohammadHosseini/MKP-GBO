4eaf977d48edb828d9e8c71a4548d6be976c7ab3
ImMohammadHosseini <sayedmh50@gmail.com>
2023-08-22 16:14:55 +0330

    add critic and value models

--- solver/src/models/mlp.py
+++ solver/src/models/mlp.py
@@ -5,17 +5,77 @@
 import torch
 import torch.nn as nn
 from typing import List, Optional
+from torch.distributions.normal import Normal
 
 
-class MLP(nn.Module):
+
+class actor_MLP(nn.Module):
     def __init__(self, 
         rowNum: int, 
         colNum: int,
         hidden_dims: List = [254, 128, 128, 64],
         device = torch.device('cpu'),
-        name = 'mlp'
+        name = 'actor_MLP'
     ):
-        super(MLP, self).__init__()
+        super(actor_MLP, self).__init__()
+        self.name = name
+        self.device = device
+        self.rowNum = rowNum
+        self.colNum =colNum
+        self.reparam_noise = 1e-6
+        
+        modules = []
+        input_dim = rowNum * colNum
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(input_dim, out_features=h_dim),
+                    nn.Softplus())
+            )
+            input_dim = h_dim
+        self.mlp = nn.Sequential(*modules).to(self.device)
+        
+        self.mu = nn.Linear(input_dim, 1, device=self.device)
+        self.sigma = nn.Linear(input_dim, 1, device=self.device)
+        
+
+    def forward(self, x):
+        x = x.flatten()
+        x = self.mlp(x)
+        
+        mu = self.mu(x)
+        sigma = self.sigma(x)
+
+        sigma = torch.clamp(sigma, min=self.reparam_noise, max=1)
+
+        return mu, sigma
+        
+    
+    def sample_normal(self, observation, reparameterize=True):
+        mu, sigma = self.forward(observation)
+        probabilities = Normal(mu, sigma)
+
+        if reparameterize:
+            actions = probabilities.rsample()
+        else:
+            actions = probabilities.sample()
+
+        action = torch.sigmoid(actions).to(self.device)
+        log_probs = probabilities.log_prob(actions)
+        log_probs -= torch.log(1-action.pow(2)+self.reparam_noise)
+        log_probs = log_probs.sum(1, keepdim=True)
+
+        return action, log_probs
+    
+class Value(nn.Module):
+    def __init__(self, 
+        rowNum: int, 
+        colNum: int,
+        hidden_dims: List = [254, 128, 128, 64],
+        device = torch.device('cpu'),
+        name = 'value_model'
+    ):
+        super(Value, self).__init__()
         self.name = name
         self.device = device
         
@@ -29,10 +89,43 @@ class MLP(nn.Module):
             )
             input_dim = h_dim
         self.mlp = nn.Sequential(*modules).to(self.device)
+        
         self.outer = nn.Linear(input_dim, 1, device=self.device)
-        self.sigmoid =  nn.Sigmoid()
-
-    def forward(self, x):
-        x = x.flatten()
-        x = self.mlp(x)
-        return self.sigmoid(self.outer(x))
+        
+    def forward(self, observation):
+        observation = observation.flatten()
+        observation = self.mlp(observation)
+        observation = self.outer(observation)
+        return observation
+    
+class Critic(nn.Module):
+    def __init__(self, 
+        rowNum: int, 
+        colNum: int,
+        hidden_dims: List = [254, 128, 128, 64],
+        device = torch.device('cpu'),
+        name = 'critic_model'
+    ):
+        super(Critic, self).__init__()
+        self.name = name
+        self.device = device
+        
+        modules = []
+        input_dim = rowNum * colNum
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(input_dim, out_features=h_dim),
+                    nn.Softplus())
+            )
+            input_dim = h_dim
+        self.mlp = nn.Sequential(*modules).to(self.device)
+        
+        self.outer = nn.Linear(input_dim, 1, device=self.device)
+        
+    def forward(self, observation, action):
+        observation = observation.flatten()
+        observation = torch.cat([observation, action], dim=1)
+        observation = self.mlp(observation)
+        observation = self.outer(observation)
+        return observation
