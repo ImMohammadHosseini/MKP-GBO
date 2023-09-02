bcb1563695a5f9dea836ada6cd84775ebf34aca7
ImMohammadHosseini <sayedmh50@gmail.com>
2023-09-02 11:19:11 +0330

    fix .grad bug, complete step method

--- solver/src/models/RL_trainer/env_.py
+++ solver/src/models/RL_trainer/env_.py
@@ -7,19 +7,20 @@ import numpy as np
 
 import gymnasium as gym
 from gymnasium import spaces
-from src.data_structure.state_prepare import StatePrepare
 from copy import deepcopy
 
 class KnapsackAssignmentEnv (gym.Env):
     def __init__ (
         self,
+        no_change_times: int = 10,
     ):        
         super().__init__()
+        self.no_change_times = no_change_times
         
         
     def setStatePrepare (
         self,
-        stateDataClasses: StatePrepare,
+        stateDataClasses,
     ):
         self.statePrepare = stateDataClasses
         
@@ -36,11 +37,10 @@ class KnapsackAssignmentEnv (gym.Env):
         self.statePrepare.reset()
         observation = self._get_obs()
         
-        self.observation = torch.tensor(observation, dtype=torch.float32,
-                                   device=self.device)
+        observation = torch.tensor(observation, dtype=torch.float32)#self.
         info = self._get_info()
 
-        return observation, info
+        return observation, info#self.
     
     def convertToOneHot(self, dat, fixed_part, ksNum):
         alloc = []
@@ -50,28 +50,36 @@ class KnapsackAssignmentEnv (gym.Env):
         new_dat_oneHot = torch.cat((fixed_part, torch.FloatTensor(alloc)), dim=1)
         return new_dat_oneHot
     
-    def step (self, action):
-        new_matrix = deepcopy(self.observation)
-        new_matrix.requires_grad = True
-        optimizer = torch.optim.Adam([new_matrix] , lr=0.8)
+    def step (self, observation, action, model):
+        #print('1',observation)
+        optimizer = torch.optim.Adam([observation] , lr=0.8)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
-        iteration = 0; equal = 0
-        fixed_part = deepcopy(new_matrix.data[:,0:-(self.statePrepare.knapsackObsSize+1)])
-        alloc_old = deepcopy(new_matrix.data[:,-(self.statePrepare.knapsackObsSize+1):])
+        fixed_part = deepcopy(observation.data[:,0:-(self.statePrepare.knapsackObsSize+1)])#self.
+        alloc_old = deepcopy(observation.data[:,-(self.statePrepare.knapsackObsSize+1):])#self.
+        #a, _ = model.forward(observation)#self.
+        #print('r', observation.requires_grad )#self.
+        #print(observation.grad)
+        #print(observation.is_leaf)
+
         optimizer.zero_grad(); action.backward(); optimizer.step(); scheduler.step()
-        new_matrix.data = self.convertToOneHot(new_matrix.data, fixed_part, self.statePrepare.knapsackObsSize+1)
-        new_matrix.requires_grad = False 
-        decision = []
-        for i, row in enumerate(new_matrix): 
-            decision_part = row[-(self.statePrepare.knapsackObsSize+1):].tolist()
-            if i == self.statePrepare.instanceObsSize:
-                break
-            if decision_part[-1] == 1:
-                continue
-            decision.append((i, decision_part.index(max(decision_part))))
+        #print('grad',observation.grad)
+        #print(observation)
+        observation.data = self.convertToOneHot(observation.data, fixed_part, self.statePrepare.knapsackObsSize)
+        #print('2',observation)
         
+        decision = []
+        for inst_id, row in enumerate(observation): 
+            decision_part = row[-(self.statePrepare.knapsackObsSize+1):].cpu().detach().numpy()
+            if inst_id == self.statePrepare.instanceObsSize:
+                break
+            if decision_part[-1] == 1 or (decision_part==0.0).all():
+                continue
+            ks_id = np.where(decision_part == 1)[0][0]
+            decision.append((inst_id, ks_id))
         reward = self.reward(decision)
-        self.statePrepare.updateMatrix(new_matrix)
+        if reward == 0:
+            self.no_change += 1
+        self.statePrepare.updateMatrix(observation)
         
         info = self._get_info()
         
@@ -79,14 +87,14 @@ class KnapsackAssignmentEnv (gym.Env):
         
         self.observation = torch.tensor(self.observation, dtype=torch.float32)
         
-        
+        if self.no_change == self.no_change_times: terminated = True
+        else: terminated = False
         
         return self.observation, reward, terminated, info
     
-    
-    
     def reward (self, decisions):
         newScore = self.statePrepare.getScore(decisions)
+        
         reward = newScore - self.score
         self.score = newScore
         return reward
